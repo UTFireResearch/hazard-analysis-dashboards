0c749660901c224fb43eeaab5330fdfe20ab44d0
Sam Matthews <37713491+smatthews95@users.noreply.github.com>
2021-04-28 00:35:36 -0500

    Add intermediate mesh sizing code

--- dashboards/apps/mesh_size.py
+++ dashboards/apps/mesh_size.py
@@ -2,8 +2,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_table
-from dash.dependencies import Input, Output
 
+from app import app
+from dash.dependencies import Input, Output, State
+from decimal import *
+from math import sqrt
 
 #----------------------APPlICATION LAYOUT---------------------------------------
 layout = html.Div(
@@ -69,42 +72,7 @@ layout = html.Div(
         #---------------START INSTRUCTION FLEX-ROW------------------------------
         html.Div(
             [
-                html.Div(
-                    [   html.H5(
-                            html.B('Instructions'),
-                            style={"margin-bottom": "15px", "color": "maroon"},
-                        ),
-                        html.H6(
-                            html.B('About This Tool'),
-                            style={"margin-left": "20px"}
-                        ),
-                        html.P(
-                            'This tool allows you to easily generate a MESH line for input into FIRE dynamics Simulator (FDS). It automatically calculates the optimal (Poison-friendly mesh division numbers and returns a complest MESH line to be used in an FDS input file. The cell sizes are determined using the characteristic fire diameter and cell size ratio that should accurately resolve your fire simulation based on the total heat release rate.',
-                            style={"margin": "10px", "font-size": "20", "margin-left": "20px"}
-                        ),
-                        html.H6(
-                            html.B('Background'),
-                            style={"margin-left": "20px"}
-                        ),
-                        html.P(
-                            'The cell size (dx) for a given simulation can be related to the characteristic fire diameter (D*), i.e., the smaller the characteristic fire diameter, the smaller the cell size should be in order to adequatey resolve the fluid flow and fire dynamics.',
-                            style={"margin": "10px", "font-size": "20", "margin-left": "20px"}
-                        ),
-                        html.P(
-                            'The characteristic fire diameter (D*) is given by the following relationship:',
-                            style={"margin": "10px", "font-size": "20", "margin-left": "20px"}
-                        ),
-                        html.Img(
-                            src=('/assets/mesh_eq1_mod.PNG'),
-                            style={'height': '100px', 'margin-left': '20px'}
-                        ),
-                        html.P(
-                            'A reference within the FDS User Guide (Verification and Validation of Selected Fire Models for Nuclear power Plant Applications. NUREG 1824, United States Nuclear Regulatory Commission, 2007) used a D*/dx ratio between 4 and 16 to accurately resolve fires in various scenarios. From the FDS User Guide: "These values were used to adequately resolve plume dynamics, along with other geometrical characteristics of the models as well. This range does not indicate what values to use for all models, only what values worked well for that particular set of models."',
-                            style={"margin": "10px", "font-size": "20", "margin-left": "20px"}
-                        ),
-                    ],
-                    className='pretty_container twelve columns'
-                ),
+
             ],
             className= "row flex-display",
         ),
@@ -297,7 +265,7 @@ layout = html.Div(
                                     style={'margin-right':'105px'}
                                 ),
                                 dcc.Input(
-                                id='mesh_desnity_in',
+                                id='mesh_density_in',
                                 type='number',
                                 placeholder='kW/m^3',
                                 size='15',
@@ -408,12 +376,66 @@ layout = html.Div(
                         ),
 
                     ],
-                    className='pretty_container twelve columns'
+                    className='pretty_container five columns'
+                ),
+                html.Div(
+                    [   html.H5(
+                            html.B('Instructions'),
+                            style={"margin-bottom": "15px", "color": "maroon"},
+                        ),
+                        html.H6(
+                            html.B('About This Tool'),
+                            style={"margin-left": "20px"}
+                        ),
+                        html.P(
+                            'This tool allows you to easily generate a MESH line for input into FIRE dynamics Simulator (FDS). It automatically calculates the optimal (Poison-friendly mesh division numbers and returns a complest MESH line to be used in an FDS input file. The cell sizes are determined using the characteristic fire diameter and cell size ratio that should accurately resolve your fire simulation based on the total heat release rate.',
+                            style={"margin": "10px", "font-size": "20", "margin-left": "20px"}
+                        ),
+                        html.H6(
+                            html.B('Background'),
+                            style={"margin-left": "20px"}
+                        ),
+                        html.P(
+                            'The cell size (dx) for a given simulation can be related to the characteristic fire diameter (D*), i.e., the smaller the characteristic fire diameter, the smaller the cell size should be in order to adequatey resolve the fluid flow and fire dynamics.',
+                            style={"margin": "10px", "font-size": "20", "margin-left": "20px"}
+                        ),
+                        html.P(
+                            'The characteristic fire diameter (D*) is given by the following relationship:',
+                            style={"margin": "10px", "font-size": "20", "margin-left": "20px"}
+                        ),
+                        html.Img(
+                            src=('/assets/mesh_eq1_mod.PNG'),
+                            style={'height': '100px', 'margin-left': '20px'}
+                        ),
+                        html.P(
+                            'A reference within the FDS User Guide (Verification and Validation of Selected Fire Models for Nuclear power Plant Applications. NUREG 1824, United States Nuclear Regulatory Commission, 2007) used a D*/dx ratio between 4 and 16 to accurately resolve fires in various scenarios. From the FDS User Guide: "These values were used to adequately resolve plume dynamics, along with other geometrical characteristics of the models as well. This range does not indicate what values to use for all models, only what values worked well for that particular set of models."',
+                            style={"margin": "10px", "font-size": "20", "margin-left": "20px"}
+                        ),
+                    ],
+                    className='pretty_container nine columns'
                 ),
             ],
             className='row flex-display',
         ),
-        #----------------BOTTOM ACKNOWLEDGEMENTS ROW---------------------------
+        #-------------MESH RESULTS ROW FLEX-DISPLAY-----------------------------
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.P(
+                            'This is a test', id='meshROut'
+                        ),
+                        html.P(
+                            'This is a test', id='meshROut2'
+                        )
+                    ],
+                    className='pretty_container twelve columns'
+                ),
+            ],
+            className='row flex-display',
+            id='mesh_results_row',
+        ),
+        #----------------BOTTOM ACKNOWLEDGEMENTS ROW----------------------------
         html.Div(
             [
                 html.Div(
@@ -429,5 +451,190 @@ layout = html.Div(
             className= "row flex-display",
 
         ),
+        #------HTML DIVS TO HOLD HIDDEN VALUES FOR DASHBOARD FUNCTIONING--------
+        html.Div(id='mesh_comp_state1',children='FALSE',style={"display": "none"}),
+        html.Div(id='mesh_comp_state2',children='FALSE',style={"display": "none"}),
+
+        #-----VALUE PASSING COMPONENTS TO SUBSTITUTE FOR GLOBAL VARIABLES-----------
+        #html.Div(id='mesh_input_fields',children='FALSE',style={"display": "none"}),
+        dcc.Store(id='input_fields', storage_type='session'),
+        dcc.Store(id='input_values', storage_type='session'),
+        dcc.Store(id='ijk_bars', storage_type='session'),
+    ],
+), #---------------------END LAYOUT---------------------------------------------
+
+
+
+#--------------------NON-CALLBACK FUNCTION DEFINITIONS--------------------------
+#-------------------------------------------------------------------------------
+#----THESE FUNCTIONS ARE MORE OR LESS TRANSCRIBED FROM THE ORIGINAL SOURCE
+
+#Calculates the deisred I,J,K values and sends them to the Poisson optimizer for
+#checking/calculation
+def dstar_compute(mesh_hrr, mesh_p_inf, mesh_cp, mesh_t_inf, mesh_grav):
+
+    mesh_hrr = float(mesh_hrr)
+    mesh_p_inf = float(mesh_p_inf)
+    mesh_cp = float(mesh_cp)
+    mesh_t_inf = float(mesh_t_inf)
+    mesh_grav = float(mesh_grav)
+
+    mesh_dstar_pwr = float("0.4")
+
+    mesh_d_star = pow( (mesh_hrr / (mesh_p_inf * mesh_cp * mesh_t_inf * mesh_grav)), mesh_dstar_pwr )
+
+    #HTML PRINTING CODE IN ORIGINAL SOURCE
+
+    mesh_uguide_min_suggested_dx = mesh_d_star / 16
+    mesh_uguide_mod_suggested_dx = mesh_d_star / 10
+    mesh_uguide_max_suggested_dx = mesh_d_star / 4
+
+    #HTML PRINTING CODE IN ORIGINAL SOURCE
+
+    mesh_uguide_min_suggested_dx = str(mesh_uguide_min_suggested_dx)
+    mesh_uguide_mod_suggested_dx = str(mesh_uguide_mod_suggested_dx)
+    mesh_uguide_max_suggested_dx = str(mesh_uguide_max_suggested_dx)
+
+    #TO BE CONTINUED -------------
+
+    return None
+
+def Poisson(mesh_desired_num):
+
+    #Uses mod 2, 3, 5 to see if numbers are Poisson optimized.
+    #----------------
+    #If they are not, it goes from x to x*1.4 in increments of 1 and rechecks
+    #until factorable
+    #----------------
+    #Then it returns the factorable and Poisson-friendly I,J,K values
+
+    for i in range(mesh_desired_num*Decimal('1.4')):
+
+        global mesh_ijk_bars
+        mesh_check_digit = mesh_desired_num
+
+        while mesh_check_digit % 2 == 0:
+            mesh_check_digit = mesh_check_digit / 2
+
+        while mesh_check_digit % 3 == 0:
+            mesh_check_digit = mesh_check_digit / 3
+
+        while mesh_check_digit % 5 == 0:
+            mesh_check_digit = mesh_check_digit / 5
+
+        if mesh_check_digit in (1, 2, 3, 5):
+            mesh_ijk_bars.append(int(mesh_desired_num))
+            break
+        else:
+            mesh_desired_num = mesh_desired_num + 1
+            continue
+
+    if len(mesh_ijk_bars) == 3:
+        print_output_results()
+        mesh_resolution = ''
+
+        #------CREATE STRING AND SEND IT TO BODY IN ORIGINAL CGI SCRIPT -
+        mesh_output_string1 = "Your MESH line for FDS is: " + str(mesh_ijk_bars[0]) + "," + str(mesh_ijk_bars[1]) + "," + str(ijk_bars[2]) + ", XB=" + str(mesh_x0_dim) + "," + str(mesh_y0_dim) + "," + str(mesh_y1_dim) + "," + str(mesh_z0_dim) + "," + str(mesh_z1_dim)
+
+        mesh_ouput_string2 = "You entered: "
+
+        return None
+
+# def print_output_results():
+
+
+
+#------------------BEGIN CALLBACK DEFINITIONS-----------------------------------
+#-------------------------------------------------------------------------------
+
+@app.callback(
+    Output('meshROut2','children'),
+    [
+        Input('submit_cell','n_clicks'),
+    ],
+    [
+        State('mesh_HRR_in','value'),
+        State('mesh_density_in','value'),
+        State('mesh_CP_in','value'),
+        State('mesh_T_in','value'),
+        State('mesh_g_in','valuel'),
     ]
-), #---------------------END LAYOUT--------------------------------------------
+)
+def exec1(mesh_btn2, mHRR, mDen, mCP, mT, mG):
+
+    if mHRR and mDen and mCP and mT and mG:
+
+        d_star_power = 0.4
+        mesh_d_star = pow( (mHRR / (mDen * mDen * mT * mG)), d_star_power )
+
+        mOstring1 = 'The characteristic fire diameter D\u2082 ' + str(round(d_star,3))
+
+        mesh_uguide_min_suggested_dx = mesh_d_star / 16
+        mesh_uguide_mod_suggested_dx = mesh_d_star / 10
+        mesh_uguide_max_suggested_dx = mesh_d_star / 4
+
+
+        return mOstring1
+
+    else:
+        return 'Fail'
+
+#---THIS CALLBACK TOGGLES VISIBILITY FOR THE RESULT ROW ON AND OFF--------------
+@app.callback(
+    Output('mesh_results_row','style'),
+    [
+        Input('mesh_comp_state1','children'),
+        Input('mesh_comp_state2','children')
+    ]
+)
+def mesh_visibility_toggle(Mcomp1,Mcomp2):
+
+    display_style = {
+        "margin-bottom": "15px",
+        "color": "maroon"
+        }
+
+    hidden_style = {
+        "margin-bottom": "15px",
+        "color": "maroon",
+        "display": "none"
+        }
+
+    btn_hidden = {'display':'none'}
+
+    btn_shown = {'display':'block'}
+
+    if Mcomp1 == 'TRUE' and Mcomp2 == 'TRUE':
+
+        return display_style
+
+    else:
+        return hidden_style
+
+#---THIS CALLBACK EXECUTES ON CLICKING OF 'CALCULATE MESH LINE' BUTTON----------
+@app.callback(
+    [
+        Output('meshROut','children'),
+        Output('mesh_comp_state1', 'children'),
+        Output('mesh_comp_state2', 'children'),
+    ],
+    [
+        Input('submit_mesh','n_clicks'),
+    ],
+    [
+        State('X_min','value'),
+        State('X_max','value'),
+        State('Y_min','value'),
+        State('Y_max','value'),
+        State('Z_min','value'),
+        State('Z_max','value'),
+        State('cell_size','value'),
+    ]
+)
+def mesh_primary_execution(mesh_btn1,xMin,xMax,yMin,yMax,zMin,zMax,cSize):
+
+
+
+
+
+    return 'Success-ish', 'TRUE', 'TRUE'

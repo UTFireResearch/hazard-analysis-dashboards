8f5a2644d7b8aff30f98f6b93ffe7c06d7902aed
Sam Matthews <37713491+smatthews95@users.noreply.github.com>
2021-05-28 17:03:03 -0500

    Update all files to remove vestigial references to dash bootstrap

--- dashboards/apps/flame_size.py
+++ dashboards/apps/flame_size.py
@@ -1,6 +1,6 @@
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
+#import dash_bootstrap_components as dbc
 import numpy as np
 import pandas as pd
 import dash_table as dt
--- dashboards/apps/steel_heating.py
+++ dashboards/apps/steel_heating.py
@@ -1,6 +1,6 @@
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
+#import dash_bootstrap_components as dbc
 import numpy as np
 import dash_table as dt
 import pandas as pd
